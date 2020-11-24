30169487c5672e06b26d331e446d5ae3952c50d5
amiedawn <amie.d.carroll@gmail.com>
2020-11-23 17:37:39 -0700

    add health prototypes

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -39,3 +39,35 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+// test to get information about the player's health
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+})
+
+// method to check if the player is alive
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+// method to see if the correct amount of health is being subtracted from the Player health property
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
